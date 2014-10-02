a1d69cbb312035233a23f8babf33cf18c030d7de
Konner <konsherman@fullsail.edu>
2014-10-02 13:25:23 -0400

    update - day 2

---
+++ Day2/HTML5 Adv/js/Compositing.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	var theCanvas = document.getElementById('Canvas1');
+	if (theCanvas && theCanvas.getContext) {
+	var ctx = theCanvas.getContext("2d");
+	if (ctx) {
+		
+			// Let's create an array of all our compositing methods
+			var compMethods = ["source-over", "source-in", "source-out", "source-atop", "lighter", "xor", "destination-over", "destination-in", "destination-out", "destination-atop", "darker", "copy" ];
+			
+		
+			
+					
+											
+		}
+	}
+}
